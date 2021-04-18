7c576b98e2842c0f589848f85e655ebfda3b3bfe
Deepali Garg <deepaligarg.chd@gmail.com>
2021-04-18 13:46:57 -0400

    fix models case

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 var PORT = process.env.PORT || 3000;
-var db = require("./Models");
+
+// Database models
+var db = require("./models");
 
 var app = express();
 app.use(express.urlencoded({ extended: true }));
