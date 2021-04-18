2ef1d3baf1cf77b03c50e32bcb690f5683991d37
Deepali Garg <deepaligarg.chd@gmail.com>
2021-04-18 14:19:42 -0400

    Fix virtual field

--- models/Workout.js
+++ models/Workout.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const opts = { toJSON: { virtuals: true } };
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
@@ -33,12 +34,12 @@ const WorkoutSchema = new Schema({
       }
     }
   ]
-});
+}, opts);
 
 WorkoutSchema.virtual("totalDuration").get(function() {
-  return this.exercises.reduce((total, exercise) => {
-    return total + exercise.duration;
-  }, 0);
+  return this.exercises.reduce(function (total, exercise) {
+      return total + exercise.duration;
+    }, 0);
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
