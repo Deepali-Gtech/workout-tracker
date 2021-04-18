e6901b5e742a25e1df638af7deed9c11b4b9362b
Deepali Garg <deepaligarg.chd@gmail.com>
2021-04-18 13:39:20 -0400

    deploying

--- server.js
+++ server.js
@@ -13,8 +13,13 @@ app.use(express.static("public"));
 require("./routes/routes.js")(app);
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workout",
-  { useNewUrlParser: true }
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
 );
 
 app.listen(PORT, () => {
